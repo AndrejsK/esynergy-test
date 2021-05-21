17b9075f5636143cfced1c99f86553e5973d0816
Andrejs Ķīlis <andrejs@kilis.id.lv>
2021-05-21 16:51:28 +0300

    Implemented laravel-auditing for products

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,15 +2,19 @@
 
 namespace App\Models;
 
+use OwenIt\Auditing\Contracts\Auditable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Product extends Model
+class Product extends Model implements Auditable
 {
+    // using laravel-auditing library
+    use \OwenIt\Auditing\Auditable;
+
     use HasFactory;
 
     protected $table = 'product';
 
-    // Required for the seeder to use mass asignment
+    // Required for the seeder to use mass assignment
     protected $fillable = ['name', 'amount', 'price'];
 }
