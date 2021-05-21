ca81a09c93134c91de6b68996e5f126de8eed631
Andrejs Ķīlis <andrejs@kilis.id.lv>
2021-05-21 13:27:16 +0300

    initial commit with laravel breeze installed

---
+++ database/migrations/2021_05_21_101658_create_product_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->integer('amount');
+            // price is limited to 999999.99
+            $table->decimal('price', 8, 2, true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
